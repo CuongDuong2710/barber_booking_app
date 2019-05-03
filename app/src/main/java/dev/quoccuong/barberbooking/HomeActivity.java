65de3723265689f4fe06dc30da3eadd0fff9aa4c
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-05-03 23:46:45 +0700

    create Home activity

---
+++ app/src/main/java/dev/quoccuong/barberbooking/HomeActivity.java
@@ -0,0 +1,14 @@
+package dev.quoccuong.barberbooking;
+
+import android.quoccuong.barberbooking.R;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
