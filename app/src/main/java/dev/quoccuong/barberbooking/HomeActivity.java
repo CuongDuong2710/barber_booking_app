871962d878c08e98d2ea8e52c1fc02f74b699f94
cuongdnq2710 <cuong.duong@titancorpvn.com>
2019-05-04 11:08:08 +0700

    PART 2: create new Fragments/ check user exist from Firestore

--- app/src/main/java/dev/quoccuong/barberbooking/HomeActivity.java
+++ app/src/main/java/dev/quoccuong/barberbooking/HomeActivity.java
@@ -1,14 +1,106 @@
 package dev.quoccuong.barberbooking;
 
 import android.quoccuong.barberbooking.R;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import dev.quoccuong.barberbooking.Common.Common;
+import dev.quoccuong.barberbooking.Fragment.HomeFragment;
+import dev.quoccuong.barberbooking.Fragment.ShoppingFragment;
 
 public class HomeActivity extends AppCompatActivity {
+
+    @BindView(R.id.bottom_navigation)
+    BottomNavigationView bottomNavigationView;
+
+    BottomSheetDialog bottomSheetDialog;
+
+    CollectionReference userRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ButterKnife.bind(HomeActivity.this);
+
+        // init
+        userRef = FirebaseFirestore.getInstance().collection("User");
+
+        // Check intent, if isLogin = true, enable full access
+        // If isLogin = false, just let user around shopping to view
+        if (getIntent() != null) {
+            boolean isLogin = getIntent().getBooleanExtra(Common.IS_LOGIN, false);
+
+            if (isLogin) {
+                // check if user is exists
+                AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+                    @Override
+                    public void onSuccess(final Account account) {
+                        if (account != null) {
+                            DocumentReference currentUser = userRef.document(account.getPhoneNumber().toString());
+                            currentUser.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                                @Override
+                                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                                    if (task.isSuccessful()) {
+                                        DocumentSnapshot userSnapshot = task.getResult();
+                                        if (!userSnapshot.exists()) {
+                                            showUpdateDialog(account.getPhoneNumber().toString());
+                                        }
+                                    }
+                                }
+                            });
+                        }
+                    }
+
+                    @Override
+                    public void onError(AccountKitError accountKitError) {
+                        Toast.makeText(HomeActivity.this, "" + accountKitError.getErrorType().getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+
+        // View
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            Fragment fragment = null;
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                if (menuItem.getItemId() == R.id.action_home)
+                    fragment = new HomeFragment();
+                else if (menuItem.getItemId() == R.id.action_shopping)
+                    fragment = new ShoppingFragment();
+                return loadFragment(fragment);
+            }
+        });
+    }
+
+    private boolean loadFragment(Fragment fragment) {
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, fragment).commit();
+            return true;
+        }
+        return false;
+    }
+
+    private void showUpdateDialog(String toString) {
     }
 }
